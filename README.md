c3413b54581a21d89f5205b718373214b7a5b3bd
jmjuanes <josemijuanes@gmail.com>
2015-10-17 19:51:32 +0200

    Upload v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,150 @@
-# dosql
-Easy and intuitive way to do queries to a MySQL database using only JSON
+# doSQL
+
+Easy and intuitive way to do queries to a MySQL database using only JSON.
+
+[![npm](https://img.shields.io/npm/v/dosql.svg?style=flat-square)](https://www.npmjs.com/package/dosql)
+[![npm](https://img.shields.io/npm/dt/dosql.svg?style=flat-square)](https://www.npmjs.com/package/dosql)
+
+## Install
+
+Install **doSQL** using NPM:
+
+```sh
+npm install --save dosql
+```
+
+## Usage
+
+### Connect(obj)
+
+Use this for connect to a database. For example:
+
+```javascript
+//Import
+var db = require('dosql');
+
+//Connect to database
+db.Connect({host: '<YOUR_HOST>', user: '<YOUR_USER>', pass: '<YOUR_PASS>', db: '<YOUR_DATABASE>'});
+```
+
+
+### Do(obj, callback)
+
+Run a query defined by `obj` object, and then call your `callback` function with the results.
+
+The `obj` object must contain the next keys:
+
+- `in`: string with the table where you want to do the query.
+- `do`: action
+
+`callback` will get only argument, called `result`:
+
+- `result` will be `null` if an error occurred running the query.
+- `result` will be an array if no error occurred.
+
+
+#### Examples
+
+For **select** data, you can optionally add a `where` key, containing an object with the keys that the rows must satisfy.
+
+```javascript
+//Example 1 - Select all rows from table
+
+// SELECT * FROM yourtable
+db.Do({in: 'yourtable', do: 'select'}, function(result){
+
+  //result is an array containing the selected rows
+  //If there was a problem doing the query, result will be NULL    
+
+  //Do something with "result"
+
+});
+```
+
+```javascript
+//Example2 - Select only rows that satisfy the conditions
+
+// SELECT * FROM yourtable WHERE key1="value1" AND key2="value2";
+db.Do({in: 'yourtable', do: 'select', where: {'key1': 'value1', 'key2': 'value2'}}, function(result){
+
+  //result is an array containing the selected rows
+  //If there was a problem doing the query, result will be NULL    
+
+  //Do something with "result"
+
+});
+```
+
+For **insert** data, you must add a `values` key. It can be an object with the values for insert into your table, or an array with some objects.
+
+```javascript
+//Example 1 - Insert only one row to your table
+
+// INSERT INTO yourtable (k1, k2, k3) VALUES ("v1", "v2", "v3");
+db.Do({in: 'yourtable', do: 'insert', values: {k1:'v1',k2:'v2',k3:'v3'}}, function(result){
+
+  //result will be NULL if something went wrong. Else, result will be an empty array
+
+});
+```
+
+```javascript
+//Example 2 - Insert two rows to your table
+
+// INSERT INTO yourtable (k1, k2, k3) VALUES ("v1", "v2", "v3"),("v4", "v5", "v6");
+var val = [{k1: 'v1', k2: 'v2', k3: 'v3'},{k1: 'v4', k2: 'v5', k3: 'v6'}];
+db.Do({in: 'yourtable', do: 'insert', values:val}, function(result){
+
+  //result will be NULL if something went wrong. Else, result will be an empty array
+
+});
+```
+
+For **update** rows, you must add a `set` and `where` keys.
+
+```javascript
+//Example1 - Update some rows
+
+// UPDATE yourtable SET key1="va1" WHERE key2="va2"
+db.Do({in: 'yourtable', do: 'update', set: {key1: 'va1'}, where: {key2: 'va2'}}, function(result){
+
+  //result will be NULL if something went wrong. Else, result will be an empty array
+
+});
+```
+
+For **delete** rows, you must add a `where` key:
+
+```javascript
+//Example1 - Delete some rows
+
+// DELETE yourtable WHERE key="value"
+db.Do({in: 'yourtable', do: 'delete', where: {key: 'value'}}, function(result){
+
+  //result will be NULL if something went wrong. Else, result will be an empty array
+
+});
+```
+
+## Tip
+
+You can use `if(result)` for check if something went wrong. For example,
+
+```javascript
+db.Do({in: 'yourtable', do: 'select'}, function(result){
+
+  //Check result
+  if(result)
+  {
+    //All is OK, you can now access to the rows
+  }
+  else
+  {
+    //Oups, something went wrong. See the Node logs
+  }
+});
+```
+
+## License
+
+**doSQL** is under the [MIT](LICENSE) license.
---
+++ index.js
@@ -0,0 +1,186 @@
+//Import dependencies
+var mydb = require('minsql');
+
+//Function for return an error
+function ReturnError()
+{
+  //Show the info
+  console.error('doSQL: please read the documentation before using doSQL.');
+
+  //Exit with error code
+  process.exit(1);
+
+  //Return
+  return false;
+}
+
+//Function for check the output
+function CheckCallback(error, action, results, callback)
+{
+  //Check for error
+  if(error)
+  {
+    //Show error
+    console.error('doSQL: error running ' + action + ':')
+    console.error(error);
+
+    //Run the callback with null argument
+    callback(null);
+  }
+  else
+  {
+    //Callback with the results
+    callback(results);
+  }
+}
+
+//Connect to database
+exports.Connect = function(obj)
+{
+  //Array with the arguments
+  var dbargs = ['host', 'user', 'pass', 'db'];
+
+  //Check all
+  for(var i = 0; i < dbargs.length; i++)
+  {
+    //Check if is defined
+    if(typeof obj[dbargs[i]] === 'undefined')
+    {
+      //Show error
+      console.error('doSQL: undefined "' + dbargs[i] + '" on database connect.');
+
+      //Exit
+      return ReturnError();
+    }
+  }
+
+  //Connect to minsql
+  mydb.Connect(obj.host, obj.user, obj.pass, obj.db);
+};
+
+//Do
+exports.Do = function(obj, callback)
+{
+  //Check callback
+  if(typeof callback === 'undefined')
+  {
+    //Show error
+    console.error('doSQL: you must provide a callback for the "Do" command.');
+
+    //Exit
+    return ReturnError();
+  }
+
+  //obj musth have the do argument
+  if(typeof obj.do === 'undefined')
+  {
+    //Show error
+    console.error('doSQL: undefined key "do" on database query.');
+
+    //Exit
+    return ReturnError();
+  }
+
+  //Convert do to undercase
+  obj.do = obj.do.toLowerCase();
+
+  //Check the in and named option
+  if(typeof obj.in === 'undefined' && typeof obj.named === 'undefined')
+  {
+    //Show error
+    console.error('doSQL: undefined key "in" or "named" on database query.');
+    console.error('doSQL: for "select", "update", "delete" or "insert", you must use "in" for set the table where you want to do the query.');
+    console.error('doSQL: for "create table" or "drop table", you must use "named" for define the name of the table');
+
+    //Exit
+    return ReturnError();
+  }
+  else
+  {
+    //Get the table
+    var table = (typeof obj.named === 'undefined')? obj.in : obj.named;
+  }
+
+  //Check the do option
+  if(obj.do === 'select')
+  {
+    //Check where
+    if(typeof obj.where === 'undefined'){ obj.where = null; }
+
+    //Make the query
+    mydb.Select(table, obj.where, function(err, results){ CheckCallback(err, 'select', results, callback); });
+  }
+  else if(obj.do === 'insert')
+  {
+    //Check for value
+    if(typeof obj.values === 'undefined')
+    {
+      //Show error
+      console.error('doSQL: undefined key "values" for insert into table');
+
+      //Exit
+      return ReturnError();
+    }
+
+    //Make the query
+    mydb.Insert(table, obj.values, function(err){ CheckCallback(err, 'insert', [], callback); });
+  }
+  else if(obj.do === 'update')
+  {
+    //Check for set and where
+    if(typeof obj.set === 'undefined' || typeof obj.where === 'undefined')
+    {
+      //Show error
+      console.error('doSQL: undefined key "set" or "where" for update rows in table');
+
+      //Return error
+      return ReturnError();
+    }
+
+    //Make the query
+    mydb.Update(table, obj.set, obj.where, function(err){ CheckCallback(err, 'update', [], callback); });
+  }
+  else if(obj.do === 'delete')
+  {
+    //Check where
+    if(typeof obj.where === 'undefined')
+    {
+      //Show error
+      console.error('doSQL: undefined key "where" for delete rows in table');
+
+      //Return error
+      return ReturnError();
+    }
+
+    //Make the query
+    mydb.Delete(table, obj.where, function(err){ CheckCallback(err, 'delete', [], callback); });
+  }
+  else if(obj.do === 'create table')
+  {
+    //Check for defined by
+    if(typeof obj.definedby === 'undefined')
+    {
+      //Show error
+      console.error('doSQL: undefined key "definedby" for create table');
+
+      //Return error
+      return ReturnError();
+    }
+
+    //Make the query
+    mydb.CreateTable(table, obj.definedby, function(err){ CheckCallback(err, 'create table', [], callback)});
+  }
+  else if(obj.do === 'drop table')
+  {
+    //Make the query
+    mydb.DropTable(table, function(err){ CheckCallback(err, 'drop table', [], callback); });
+  }
+  else
+  {
+    //Show error
+    console.error('doSQL: unknow action "' + obj.do + '"');
+
+    //Return error
+    return ReturnError();
+  }
+};
